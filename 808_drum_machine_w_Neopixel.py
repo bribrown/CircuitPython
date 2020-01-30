e37d50ae18d97ea54b158f54629ff0eda8a1e10d
bribrown <bribrown@yahoo.com>
2020-01-29 21:31:42 -0500

    Add files via upload

--- 808_drum_machine_w_Neopixel.py
+++ 808_drum_machine_w_Neopixel.py
@@ -1,20 +1,12 @@
-# Playground 808
-# Drum machine with Nexpixel
-from digitalio import DigitalInOut, Direction
-import audioio
 import touchio
 import board
 import time
 import neopixel
+from adafruit_circuitplayground.express import cpx
 
 bpm = 120 #beats per minute for sustained hold, change this to suit your tempo
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.2)
-
-# enable the speaker
-spkrenable = DigitalInOut(board.SPEAKER_ENABLE)
-spkrenable.direction = Direction.OUTPUT
-spkrenable.value = True
+#pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.2)
 
 # make the input cap sense pads
 capPins = (board.A1, board.A2, board.A3, board.A4, board.A5,
@@ -31,9 +23,7 @@ audiofiles = ["bd_tek.wav", "elec_hi_snare.wav", "elec_cymbal.wav",
 
 def play_file(filename):
     print("playing file "+filename)
-    f = open(filename, "rb")
-    a = audioio.AudioOut(board.A0, f)
-    a.play()
+    cpx.play_file(filename)
     time.sleep(bpm/960) #sixteenthNote delay
 
 while True:
@@ -41,11 +31,10 @@ while True:
     for i in range(7):
         if touchPad[i].value:
             if i < 3:
-                pixels[i]=(255,0,0)
+                cpx.pixels[i]=(255,0,0)
             if (i >=3 and i < 5):
-                pixels[i]=(0,255,0)
+                cpx.pixels[i]=(0,255,0)
             elif i >= 5:
-                pixels[i]=(0,0,255)
-            # pixels[i]=(255,0,0)
+                cpx.pixels[i]=(0,0,255)
             play_file(audiofiles[i])
-            pixels[i]=(0,0,0)
+            cpx.pixels[i]=(0,0,0)
